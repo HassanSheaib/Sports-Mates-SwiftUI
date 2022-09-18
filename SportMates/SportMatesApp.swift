211bd6b6a729cb42a79a4de6e78989df77e92757
HassanSheaib <hacan.sheaib@gmail.com>
2022-09-18 11:59:54 +0300

    Initial Commit

---
+++ SportMates/SportMatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportMatesApp.swift
+//  SportMates
+//
+//  Created by HHS on 18/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SportMatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
